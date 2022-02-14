43cecd88f25fdd45b79764db72326b5c93915f86
Thyago Raphael <thyagoraphael1987@gmail.com>
2022-02-13 21:40:43 -0300

    Create my Personal Blog

---
+++ Content/posts/first-post.md
@@ -0,0 +1,8 @@
+---
+date: 2022-02-13 19:02
+description: A description of my first post.
+tags: first, article
+---
+# My first post
+
+My first post's text.
---
+++ Sources/NoobdoiOSBlog/main.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Publish
+import Plot
+
+// This type acts as the configuration for your website.
+struct NoobdoiOSBlog: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case posts
+        case about
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://your-website-url.com")!
+    var name = "NoobdoiOSBlog"
+    var description = "A description of NoobdoiOSBlog"
+    var language: Language { .english }
+    var imagePath: Path? { nil }
+}
+
+// This will generate your website using the built-in Foundation theme:
+try NoobdoiOSBlog().publish(withTheme: .foundation)
